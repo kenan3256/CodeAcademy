fe661f63705aca77825cbb7eb98f06d5d956b707
Kenan <114463518+kenan3256@users.noreply.github.com>
2023-12-15 17:04:45 +0400

    15.12.2023 Lesson4

--- src/main/kotlin/Lesson04/Task/Main.kt
+++ src/main/kotlin/Lesson04/Task/Main.kt
@@ -5,24 +5,27 @@ import java.util.*
 
 
 fun main(){
-    var arrayList: Array<Shape> = arrayOf(Circle(4.5),Rectangle(2.0,5.6),Sphere(5.4))
-    println("")
+    var arrayList: ArrayList<Shape> = arrayListOf(Circle(4.5),Rectangle(2.0,5.6),Sphere(5.4))
+
     arrayList.forEach { val name=it.getName()
 
-        when(it){
-            is TwoDimensional-> println(name)
-
-
-    }
+        when (it) {
+            is TwoDimensional -> { println("$name = ${it.calculateArea()}") }
+            is ThreeDimensional -> { println("$name = ${it.calculateVolume()}") }
+            else -> throw Exception("Booom!!!!")
 
 
 
         }
 
 
-    }
 
+        }
 
 
 }
+
+
+
+
 //arrayOf(Circle(2.1),Rectangle(2.5,3.4),Sphere(5.4))
